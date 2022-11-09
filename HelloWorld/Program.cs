a209b90ef034618f2c43bd5abbea92a53838f167
Gilbert <gssenyonjo@gmail.com>
2022-11-10 00:40:24 +0300

    looked at unicode

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,22 +7,15 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            BigInteger i1 = 1;
-            BigInteger i2 = 1;
+            char[] chars = {'c', 'a', 'f', (char) 0xe9, 's'};
+            string text = new string(chars);
+            Console.WriteLine(text);
+            Console.WriteLine(text.Length);
+            Console.WriteLine(new string(text.Reverse().ToArray()));
 
-            Console.WriteLine(i1);
-            int count = 0;
-
-            while(true && count < 400_000)
-            {
-                if(count++ % 100_000 == 0)
-                {
-                    Console.WriteLine("i2 is " + i2);
-                }
-                BigInteger next = i1 + i2;
-                i1 = i2;
-                i2 = next;
-            }
+            string t2 = "cafés";
+            Console.WriteLine(t2);
+            Console.WriteLine(t2.Length);
         }
 
     }
