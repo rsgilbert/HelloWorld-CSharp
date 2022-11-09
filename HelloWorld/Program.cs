54b414317666b2480af8ca0869cdd7700dbe2fe5
Gilbert <gssenyonjo@gmail.com>
2022-11-10 00:25:01 +0300

    wrote fibonacci program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,29 +1,34 @@
 ﻿using System.Diagnostics;
-
+using System.Numerics;
 
 namespace HelloWorld
 {
     public class Program
     {
-        [Conditional("RELEASE")]
-        static void ShowDebugInfo()
-        {
-            Console.WriteLine("I am allowed to show debug info2");
-        }
-     
         public static void Main(string[] args)
         {
-#pragma warningm disable
-            var i = 10;
+            BigInteger i1 = 1;
+            BigInteger i2 = 1;
 
-            Console.WriteLine("Done 29");
-            ShowDebugInfo();
-#if false
-#error We do not support this version
-#endif
+            Console.WriteLine(i1);
+            int count = 0;
+
+            while(true && count < 400_000)
+            {
+                if(count++ % 100_000 == 0)
+                {
+                    Console.WriteLine("i2 is " + i2);
+                }
+                BigInteger next = i1 + i2;
+                i1 = i2;
+                i2 = next;
+            }
         }
+
     }
+    
 }
+
 
 
 
