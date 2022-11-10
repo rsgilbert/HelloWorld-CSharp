9c4eab2d81cbe09a3f89e4f27c7fecbf97bb62cb
Gilbert <gssenyonjo@gmail.com>
2022-11-10 20:44:07 +0300

    looked at ternary operator

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,35 +7,15 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            (int X, int Y) point = (1, 4);
-            Console.WriteLine($"X is {point.X}, Y is {point.Y}");
+            int x = 3;
+            int y = 5;
+            int mx = (x > y) ? x : y;
+            Console.WriteLine(mx);
 
-            var sides = (right: 10, left: 5, top: 15, bottom: 4);
-            Console.WriteLine(sides);
-            Console.WriteLine($"Perimeter = {sides.right + sides.top + sides.bottom + sides.left}");
-
-            string firstname = "John";
-            string lastname = "Walker";
-
-            var names = (firstname, lastname);
-            Console.WriteLine(names);
-
-            (int, int, string) info = (2, 123, "James");
-            Console.WriteLine($"{info.Item1}, {info.Item2}, {info.Item3}");
-
-            (int p2, int q2) point2 = point;
-            Console.WriteLine(point2.q2);
-
-
-            // deconstruction
-            (double l, double w, double h) cube = (1, 2, 3);
-
-            (double l2, double w2, double h2) = cube;
-
-            Console.WriteLine(w2);
-            double n, m, o;
-            (n, m, o) = cube;
-            Console.WriteLine(o);
+            string msg = null;
+            string finalMsg = msg ?? "No message";
+            Console.WriteLine(finalMsg);
+            Console.WriteLine(msg?.Length ?? -1);
 
 
         }
