4a6b3daf67e10ec199ebaa7e943aa0f4cb47e45f
Gilbert <gssenyonjo@gmail.com>
2022-11-12 20:02:11 +0300

    looked at structs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,56 +1,102 @@
 ﻿using static System.Console;
 using static System.Math;
+
+
+
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        string? texty2 = null;
-        string t3 = texty2!; // null forgiving operator
+        var p1 = new Point(40, 2);
+        Point p2 = p1;
+        var p3 = new Point(40, 2);
 
-    
-        if(texty2 != null)
-        {
-            WriteLine($"L is {texty2.Length}");
-        }
-        WriteLine($"Length is {texty2?.Length ?? -1}");
-        // WriteLine($"L2 is {texty2!.Length}");
+        Console.WriteLine($"{p1.X}, {p1.Y}");
+        Console.WriteLine($"{p2.X}, {p2.Y}");
+        Console.WriteLine($"{p3.X}, {p3.Y}");
 
-        var nullableStrings = new string?[5];
-        var nonNullableStrings = new string[5];
-
-        WriteLine($"nullable [1] {nullableStrings[1]}");
-        WriteLine($"non-nullable [1] {nonNullableStrings[1]}"); 
-
+        WriteLine(p1 == p2);
+        WriteLine(p2 == p3);
+        WriteLine(object.Equals(p3, p2));
+        
+        var p5 = new Point2(8, 5);
+        var p6 = new Point2(8, 5);
+        
+        
+        Console.WriteLine($"{p5.X}, {p5.Y}");
+        Console.WriteLine($"{p6.X}, {p6.Y}");
+        WriteLine(p5 == p6);
+        // WriteLine(object.ReferenceEquals(p1, p2));
     }
 
-    public static string Get(IDictionary<int, string> d)
-    {
-        if(d.TryGetValue(42, out string? s))
-        {
-            return s;
-        }
-        return "Not found";
-    }
 }
 
-public class Counter
+public readonly struct Point2 
 {
-    private int _count;
-    private static int _totalCount;
-
-    public int GetNextValue()
+    public Point2(double x, double y)
     {
-        this._count += 1;
-        _totalCount += 1;
-        return _count;
+        X = x;
+        Y = y;
     }
-    public int Count => _count;
+    public double X { get; }
+    public double Y { get; }
 
-    public static int TotalCount => _totalCount;
-
-    public static int GetTotalCount()
+    public double DistanceFromOrigin()
     {
-        return _totalCount;
+        return Math.Sqrt(X * X + Y * Y);
     }
-
+    public static bool operator ==(Point2 p1, Point2 p2)
+    {
+        return p1.X == p2.X && p1.Y == p2.Y;
+    }
+    public static bool operator !=(Point2 p1, Point2 p2)
+    {
+        return !(p1 == p2);
+    }
+     public override bool Equals(object? o)
+    {
+        return o is Point2 p2 && this == p2;
+    }
+    public override int GetHashCode()
+    {
+        return (X, Y).GetHashCode();
+    }
 }
+
+
+public struct Point 
+{
+    private double _x;
+    private double _y;
+
+    public Point(double x, double y)
+    {
+        _x = x;
+        _y = y;
+    }
+    public double X => _x;
+    public double Y => _y;
+
+    public static bool operator ==(Point p1, Point p2)
+    {
+        return p1.X == p2.X && p1.Y == p2.Y;
+    }
+    public static bool operator !=(Point p1, Point p2)
+    {
+        return !(p1 == p2);
+    }
+
+    public override bool Equals(object? o)
+    {
+        return o is Point p2 && this == p2;
+    }
+    public override int GetHashCode()
+    {
+        return (X, Y).GetHashCode();
+    }
+}
+
+
+
