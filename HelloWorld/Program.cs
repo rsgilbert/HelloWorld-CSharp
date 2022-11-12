a0307f91da7e5fb78b313babad773d4e3fe808c9
Gilbert <gssenyonjo@gmail.com>
2022-11-12 04:40:03 +0300

    created Counter class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,71 +1,43 @@
 ﻿using System.Diagnostics;
 using System.Numerics;
+ 
 
-namespace HelloWorld
-{
-    public class Program
+ // a declaration space is a region of code in which a single name must not refer to 
+ // two different entities
+ public class Counter 
+ {
+    private int _count;
+    private static int _totalCount;
+
+    public  int  GetNextValue()
     {
-        public static void Main(string[] args)
-        {
-            object o = (3, 3);
-            
-            switch(o)
-            {
-                case string :
-                    Console.WriteLine($"string is {o}");
-                    break;
-                case int i:
-                    Console.WriteLine($"integer is {i}");
-                    break;
-                case double d:
-                    Console.WriteLine($"double {d}");
-                    break;
-                case (3, int y):
-                    Console.WriteLine($"x is {3}, y is {y}");
-                    break;
-                case (int n, int m, int r):
-                break;
-            }
-
-            string msg = "Hey t";
-
-            switch(msg)
-            {
-                case { Length: 0}:
-                    Console.WriteLine("Blank");
-                    break;
-                case { Length: 3}:
-                    Console.WriteLine($"Length is 3");
-                    break;
-                case { Length: int l } when l >= 4 && l <= 6:
-                    Console.WriteLine($"Short length {l}");
-                    break;
-                case { Length: int l }:
-                    Console.WriteLine($"Length is really {l}");
-                    break;
-            }
-
-
-            string shape = "Kiter";
-            string layout = shape switch 
-            {
-                string s when s.Length == 4 => "Potrait",
-                string s when s == "Kite" => "Square",
-                _ => "Landscape"
-            };
-            Console.WriteLine(layout);
-
-            // pattern in an is expression
-            bool isString = "Hello" is string;
-            Console.WriteLine(isString);
-            
-        }
-
+        string _count = "f";
+        string _totalCount = "m";
+        this._count += 1;
+        Counter._totalCount = 1;
+        return _count;
     }
 
+    public static int TotalCount => _totalCount;
+
+    public static int GetTotalCount()
+    {
+        return _totalCount;
+    }
+
+ }
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        var c1 = new Counter();
+        var c2 = new Counter();
+        Console.WriteLine(c1.GetNextValue());
+        Console.WriteLine(c1.GetNextValue());
+
+        Console.WriteLine(c2.GetNextValue());
+
+        Console.WriteLine($"total count {Counter.GetTotalCount()}");
+    }
 }
-
-
-
-
-
