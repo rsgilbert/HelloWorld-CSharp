696e9231e90938bb02310fb54b351225c9f55d17
Gilbert <gssenyonjo@gmail.com>
2022-11-13 07:54:10 +0300

    looked at default values

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,86 +7,20 @@ using static System.Math;
 
 class Program
 {
-  
     static void Main(string[] args)
     {
-        Size s1 = new Size(5, 3);
-        Size s2 = new Size(2, 0);
-        WriteLine(Size.DiagonalLength(s1));
-        WriteLine(Size.DescribeSize(s2));
+        Blame(problem: "poverty");
 
-   
-        int q = Divide(3, 4, out  int r);
-        WriteLine($"Remainder {r}, Quotient {q}");
-
-   
-        int q2 = Divide(10, 3, out _);
-        WriteLine($"q2 {q2}");
-
-        decimal v = decimal.TryParse("12.198", out decimal result) ? result : -1;
-        WriteLine($"Parsed value is {v}");
-
-        long x = 31;
-        Interlocked.Increment(ref x);
-        WriteLine($"X {x}");
-
-        int z = 4;
-        Reduce(3, ref z);
-        WriteLine($"z is {z}");
-        
-        double dd = 12;
-        WriteLine($"Squared {Square(in dd)}, dd is still {dd}");
     }
 
-    public static double Square(in double d)
+    public static void Blame(string perpertrator = " kids ", string problem = " the dirty environment")
     {
-        var result = d * d;
-        return result;
+        Console.Write($"We blame {perpertrator} for {problem}.");
+
+        // using var r = new StreamReader(stream, Encoding.UTF8, true, 8192, false);
+        // using var r2 = new StreamReader(stream, Encoding.UTF8, detectEncodingFromByteOrderMarks: true, bufferSize:8192, leaveOpen: false);
+
+        // using var w = new StreamWriter(filepath, append:true, Encoding.UTF8);
+
     }
-
-    public static void Reduce(int byAmount, ref int n)
-    {
-        n = n - byAmount;
-    }
-
-    public static int Divide(int x, int y, out int remainder)
-    {
-        remainder = x % y;
-        return x / y;
-    }
-
-
-
-}
-
-public readonly struct Size
-{
-    public Size(double w, double h)
-    {
-        W = w;
-        H = h;
-    }
-
-    public void Deconstruct(out double w, out double h)
-    {
-        w = W;
-        h = H;
-    }
-
-    public static double DiagonalLength(Size s)
-    {
-        (double width, double height) = s;
-        return Math.Sqrt(width * width + height * height);
-    }
-
-    public static string DescribeSize(Size s) => s switch 
-    {
-        (0, 0) => "Empty",
-        (0, _) => "Very narrow",
-        (_, 0) => "Very short",
-        _ => "Normal"
-    };
-
-    public double W { get; }
-    public double H { get; }
 }
