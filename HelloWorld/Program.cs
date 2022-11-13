7240b6b863a7da0bcb6d3e8962351d3db5eb7f5b
Gilbert <gssenyonjo@gmail.com>
2022-11-13 20:18:58 +0300

    implemented generic comparer using type constraints

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,51 +6,30 @@ public partial class Program
 {
     static void Main(string[] args)
     {
-        var housePrinter = new HousePrinter<FlatHouse>();
-        var flat1 = new FlatHouse();
-        housePrinter.Print(flat1);
+        var comparer = new GenericComparer<int>();
+        int[] arr = {0, 1, 8, 3};
 
-        var simpleHousePrinter = new HousePrinter<IHouse>();
-        simpleHousePrinter.Print(new SimpleHouse("My simple house"));
-
+        Array.Sort(arr, comparer);
+        WriteLine(string.Join(",", arr));
     }
 
 }
 
-public class HousePrinter<T>
-    where T : IHouse
+
+
+public class GenericComparer<T> : IComparer<T> 
+    where T : IComparable<T> 
 {
-    public void Print(T t)
+    public int Compare(T? t1, T? t2) 
     {
-        Console.WriteLine(t.description);
+        return t1?.CompareTo(t2) ?? -1;
     }
 }
 
-
-
-public interface IHouse
+public class Foo<T1, T2> where T1 : T2 
 {
-    string description { get; set; }
-}
-
-public class FlatHouse : IHouse
-{
-    public string description { get; set; } = "This is a flat house";
 
 }
-
-
-
-public class SimpleHouse : IHouse 
-{
-    private string _description;
-    public SimpleHouse(string description)
-    {
-        _description = description;
-    }
-    public string description { get { return _description; }set{}} 
-}
-
 
 
 
