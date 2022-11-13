159bea9793438b5e23420a357a470ec1868be246
Gilbert <gssenyonjo@gmail.com>
2022-11-13 15:40:10 +0300

    looked at anonymous types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,39 +6,13 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var temp = PorridgeTemperature.TooHot;
-
-        switch (temp)
-        {
-            case PorridgeTemperature.TooHot:
-                WriteLine("too hot");
-                break;
-            case PorridgeTemperature.TooCold:
-                WriteLine("very cold");
-                break;
-        }
-        WriteLine(((int)temp));
-        WriteLine($"Just right: {(int)PorridgeTemperature.JustRight}");
-
-        WriteLine($"too cold: {(int)PorridgeTemperature.TooCold}");
-
-        PorridgeTemperature hotOrCold = PorridgeTemperature.TooCold | PorridgeTemperature.TooHot;
-        WriteLine(hotOrCold); // prints: TooHot, TooCold
-
-        var v = hotOrCold & PorridgeTemperature.TooCold;
-        WriteLine(v);
-
+        var x = new { Title = "Lord", Surname = "Voldemort"};
+        var y = new { Title = "Lord", Surname = "Voldemort"};
+        WriteLine($"Welcome, {x.Title} {x.Surname}");
+        WriteLine(x == y); // false
 
     }
 
 
-    [Flags]
-    public enum PorridgeTemperature: long
-    {
-        // order them as powers of 2 since they are bit flags
-        TooHot = 1,
-        TooCold = 2,
-        JustRight = 4
 
-    };
 }
