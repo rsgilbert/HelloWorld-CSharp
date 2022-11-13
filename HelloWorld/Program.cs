d3a3ab8c2f532d620f7072db85340c3665f2455c
Gilbert <gssenyonjo@gmail.com>
2022-11-13 09:55:15 +0300

    looked at properties including auto-properties and indexer properties

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,36 +1,86 @@
 ﻿using static System.Console;
 using static System.Math;
-using MyApp;
+using System.Windows;
 
 
-
-
-
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        string nm = "Jackie Chan";
-        nm.PrintToConsole();
-        "Magnus".PrintLength();
+        House house = new House();
+        house.Height = 100;
+        WriteLine($"House height is {house.Height}");
+        // house.Width = 5; 
+        WriteLine($"House width is {house.Width}");
+
+        House house2 = new House(5);
+        house2.Height = 3;
+        WriteLine($"Base area is {house2.BaseArea}");
+
+        Mansion m = new Mansion();
+        m.House = new House(3);
+        m.House.Height = 4;
+        WriteLine(m.House.BaseArea);
+
+        // look at indexers 
+        var hp = new HarryPotter();
+        WriteLine($"{hp[1]}");
+        WriteLine($"{hp[3]}");
     }
-
-
 
 }
 
-namespace MyApp
+// indexer
+public class HarryPotter
 {
-    public static class MyStringExt 
+    public string this[int index]
     {
-        public static void PrintToConsole(this string s) 
+        get
         {
-            Console.WriteLine(s);
+            switch (index)
+            {
+                case 1: return "Sorcerer's Stone";
+                case 2: return "Chamber of Secrets";
+                case 3: return "Prizoner of Azkaban";
+                default: throw new Exception("Unknown");
+            }
         }
-        public static void PrintLength(this string s)
-        {
-            Console.WriteLine($"The length of {s} is {s.Length}");
-        }
-        
     }
+}
+
+public struct Mansion
+{
+    public House House { get; set; }
+}
+
+public class House
+{
+    private double _height;
+
+    public House() { }
+    public House(double width)
+    {
+        Width = width;
+    }
+
+    public double Height
+    {
+        get
+        {
+            WriteLine($"_height is {_height}");
+            return _height * 1000;
+        }
+        set
+        {
+            WriteLine($"value is {value}");
+            _height = value / 1000;
+        }
+    }
+
+    public double Width { get; private set; } = -1;
+
+    public double BaseArea => Width * Height;
+
+
+
 }
