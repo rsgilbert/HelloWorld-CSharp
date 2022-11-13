b76ac7e444543af031651de6efe0f90174f7e479
Gilbert <gssenyonjo@gmail.com>
2022-11-13 14:27:34 +0300

    looking at defining an interface

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,26 +6,39 @@ public class Program
 {
     static void Main(string[] args)
     {
-        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        string[] files = Directory.GetFiles(path);
-        var comparer = new LengthComparer();
-        Array.Sort(files, comparer);
-        foreach(string s in files)
-        {
-            WriteLine(s);
-        } 
+        DoStuff ds = new DoStuff();
+        ds.Name = "James";
+        WriteLine(ds.Name);
+
+        IDoStuff ds2 = new DoStuff();
+        WriteLine(ds2.Id);
+
+        WriteLine(((IDoStuff) ds).Id );
 
     }
 
 
-    private class LengthComparer : IComparer<string>
-    {
-        public int Compare(string? x, string? y)
-        {
-            int diff = (x?.Length ?? 0) - (y?.Length ?? 0);
-            return diff == 0 ? (x?.CompareTo(y) ?? 0) : diff;
-        }
-    }
-
+  
 }
 
+public class DoStuff : IDoStuff
+{
+    private string _name = "";
+    public string this[int i] { get {return $"Index {i}";} set {} }
+    public string Name { get {return _name; } set { _name = value; }}
+    int IDoStuff.Id => 1;
+    public int Hey(string other) 
+    {
+        Console.WriteLine($"Hey {other}");
+        return 0;
+    }
+}
+
+public interface IDoStuff 
+{
+    string this[int i] { get; set; }
+    string Name { get; set; }
+    int Id { get; }
+    int Hey(string other);
+    // event EventHandler Click;
+}
