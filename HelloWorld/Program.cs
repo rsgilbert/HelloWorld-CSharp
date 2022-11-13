9e939535c27071ecd99a8a0b81cdc57a9168923e
Gilbert <gssenyonjo@gmail.com>
2022-11-13 07:27:12 +0300

    looked at passing arguments by reference (out, ref, in)

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,12 +7,52 @@ using static System.Math;
 
 class Program
 {
+  
     static void Main(string[] args)
     {
         Size s1 = new Size(5, 3);
         Size s2 = new Size(2, 0);
         WriteLine(Size.DiagonalLength(s1));
         WriteLine(Size.DescribeSize(s2));
+
+   
+        int q = Divide(3, 4, out  int r);
+        WriteLine($"Remainder {r}, Quotient {q}");
+
+   
+        int q2 = Divide(10, 3, out _);
+        WriteLine($"q2 {q2}");
+
+        decimal v = decimal.TryParse("12.198", out decimal result) ? result : -1;
+        WriteLine($"Parsed value is {v}");
+
+        long x = 31;
+        Interlocked.Increment(ref x);
+        WriteLine($"X {x}");
+
+        int z = 4;
+        Reduce(3, ref z);
+        WriteLine($"z is {z}");
+        
+        double dd = 12;
+        WriteLine($"Squared {Square(in dd)}, dd is still {dd}");
+    }
+
+    public static double Square(in double d)
+    {
+        var result = d * d;
+        return result;
+    }
+
+    public static void Reduce(int byAmount, ref int n)
+    {
+        n = n - byAmount;
+    }
+
+    public static int Divide(int x, int y, out int remainder)
+    {
+        remainder = x % y;
+        return x / y;
     }
 
 
