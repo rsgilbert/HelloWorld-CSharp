2b4a4629df3725173be6b660cde893b177b162d4
Gilbert <gssenyonjo@gmail.com>
2022-11-14 07:30:18 +0300

    looked at reference type constraint

---
+++ HelloWorld.Tests/TestBase.cs
@@ -0,0 +1,18 @@
+using Moq;
+
+public class TestBase<TSubject, TFake>
+    where TSubject : new()
+    where TFake : class
+{
+    public TSubject Subject {get; private set; }
+    public Mock<TFake> Fake { get; private set;  }
+
+    [TestInitialize]
+    public void Initialize() 
+    {
+        Subject = new TSubject();
+        Fake = new Mock<TFake>();
+    }
+}
+
+public interface ABC {}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,35 +1,26 @@
 ﻿using static System.Console;
 using static System.Math;
 using System.Windows;
+// using Microsoft.VisualBasic.T
 
 public partial class Program
 {
     static void Main(string[] args)
     {
-        var comparer = new GenericComparer<int>();
-        int[] arr = {0, 1, 8, 3};
-
-        Array.Sort(arr, comparer);
-        WriteLine(string.Join(",", arr));
+        Bar<IAge> b = new Bar<IAge>();
     }
 
 }
 
 
-
-public class GenericComparer<T> : IComparer<T> 
-    where T : IComparable<T> 
-{
-    public int Compare(T? t1, T? t2) 
-    {
-        return t1?.CompareTo(t2) ?? -1;
-    }
-}
-
-public class Foo<T1, T2> where T1 : T2 
+public class Bar<T> where T : class
 {
 
 }
+
+public interface IAge {}
+public class Age { }
+
 
 
 
