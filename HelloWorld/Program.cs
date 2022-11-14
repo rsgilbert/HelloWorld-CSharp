dbad8522d305ee1d18f41fc173ff7de90309aeff
Gilbert <gssenyonjo@gmail.com>
2022-11-14 20:06:25 +0300

    looked at ValueTuple

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,20 +7,16 @@ public partial class Program
 {
     static void Main(string[] args)
     {
-        string[] names = { "Peter", "Isaac", "Rodney" };
-        WriteLine($"Last name is {GetLast(names)}");
+        ValueTuple<int, int> p0 = (1, 4);
+        var t = (1,2,3,4,5,6,7,8,9,10,11,12);
+        WriteLine(t);
 
-        int[] ages = { 5, 3, 2 };
-        WriteLine($"Last age is {GetLast(ages)}");
-
-
+        var p1 = Pos();
+     
 
     }
 
-    public static T GetLast<T>(T[] items) => items[items.Length - 1];
-
-  
-
+    public static (int X, int Y) Pos() => (5, 3);
 
 
 }
