3404a337e2e857682a3260ac0caaf04fb7011a66
Gilbert <gssenyonjo@gmail.com>
2022-11-15 07:10:29 +0300

    looked at extracting copyright from assembly

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,22 +1,24 @@
 ﻿using static System.Console;
 using static System.Math;
 using System.Windows;
+using System.Reflection;
 
 
 public partial class Program
 {
     static void Main(string[] args)
     {
-        ValueTuple<int, int> p0 = (1, 4);
-        var t = (1,2,3,4,5,6,7,8,9,10,11,12);
-        WriteLine(t);
-
-        var p1 = Pos();
-     
-
+        WriteLine(GetCopyrightForType(new List<int>()));
     }
 
-    public static (int X, int Y) Pos() => (5, 3);
+    public static string GetCopyrightForType(object o)
+    {
+        Assembly asm = o.GetType().Assembly;
+        var copyrightAttribute = (AssemblyCopyrightAttribute) asm.GetCustomAttributes(typeof (AssemblyCopyrightAttribute), true)[0];
+        return copyrightAttribute.Copyright;
+    }
 
 
 }
+
+public class Hey {}
