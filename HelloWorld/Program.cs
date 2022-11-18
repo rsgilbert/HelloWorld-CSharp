bbd0582965190e254a28970c5785a67d3dffe13e
Gilbert <gssenyonjo@gmail.com>
2022-11-18 07:20:29 +0300

    looked at iterators

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,16 +9,48 @@ public partial class Program
 {
     static void Main(string[] args)
     {
-      var arr = new[] {"John", "Dike"};
-      IList<string> l = arr;
-      WriteLine(l.IndexOf("Dike"));
-      WriteLine(l[0]);
-      
-      IReadOnlyList<string> r = (IReadOnlyList<string>) l;
-      WriteLine(r[1]);
+        var enumerable = CountDown(7, 5);
+        var enumerator = enumerable.GetEnumerator();
+        while(enumerator.MoveNext())
+        {
+            WriteLine(enumerator.Current);
+        }
+
+        foreach(int i in ThreeNumbers())
+        {
+            WriteLine(i);
+        }
+        enumerator =  CountDown2(3, 1);
+        while(enumerator.MoveNext())
+        {
+            WriteLine(enumerator.Current);
+        }
 
     }
 
+    public static IEnumerable<int> CountDown(int start, int end)
+    {
+        for(int i = start; i >= end; i--)
+        {
+            yield return i;
+        }
+    }
+
+
+    public static IEnumerator<int> CountDown2(int start, int end)
+    {
+        for(int i = start; i >= end; i--)
+        {
+            yield return i;
+        }
+    }
+
+    public static IEnumerable<int> ThreeNumbers()
+    {
+        yield return 1;
+        yield return 2;
+        yield return 3;
+    }
 
 
 }
