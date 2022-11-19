b36cd488da7e7bb20c620da0ae3cb3c6a6a895a8
Gilbert <gssenyonjo@gmail.com>
2022-11-19 14:41:35 +0300

    Looked at dictionaries

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,72 +9,119 @@ public partial class Program
 {
     static void Main(string[] args)
     {
-       char[] letters = { 'a', 'b', 'c', 'd'};
-       char b = letters[^3];
-       int two = 2;
-       char c = letters[^two];
+        Dictionary<string, string> names = new Dictionary<string, string>();
+        names["simon"] = "Simon Peter";
+        names["gilbert"] = "Gilbert Robert";
+        names.Add("uk", "United Kingdom");
+        names["uk"] = "Uniked";
 
-       WriteLine($"{b}, {c}");
-
-       char[] sub = letters[2..3];
-       Index last = ^1;
-       WriteLine($"{sub[0]}, {sub.Length}, {^1}, {letters[last]}");
-       
-
-        var ages = new[] {10, 20, 30, 40, 50, 60};
-        var lastSub = ages[3..^0];
-        WriteLine($"{lastSub[0]}, {lastSub[2]}");
-
-        var name = "Jonathan Ions";
-        var lastName = name[(1+name.IndexOf(" "))..];
-        WriteLine(lastName);
-
-        // array segment
-        string[] colors = new[] {"red", "green", "yellow", "blue", "orange"};
-        ArraySegment<string> colorsSegment = colors;
-        ArraySegment<string> middleColors = colorsSegment[1..^1];
-        WriteLine($"{middleColors[0]}, {middleColors[1]}");
-        middleColors[0] = "Black";
-        WriteLine(colors[1]);
-
-        // span 
-        int[] tenNums = {10,11,12,13,14,15,16,17,18,19};
-        Span<int> allTen = tenNums;
-        Span<int> secondHalf = allTen[4..10];
-        WriteLine(secondHalf[^1]);
-        secondHalf[^1] = 1999;
-        ReadOnlySpan<int> readonlyFirstHalf = tenNums.AsSpan(0..5);
-        WriteLine($"first length {readonlyFirstHalf.Length}");
-        WriteLine(readonlyFirstHalf[0]);
-
-        WriteLine(new Index(3, fromEnd:true).GetOffset(5));
-
-        var myIndexable = new MyIndexable();
-        WriteLine(myIndexable[2]);
-        WriteLine(myIndexable[^2]);
-
-        var myIndexable1 = myIndexable[0..^4];
-        WriteLine(myIndexable1[^1]);
-
-
-
-    }
-
-    private class MyIndexable 
-    {
-        public char this[int index] => (char) ('0' + index);
-
-        public int Count => 5;
-
-        public MyIndexable Slice(int offset, int length) 
+        WriteLine(names["simon"]);
+        WriteLine(names["uk"]);
+        if(names.TryGetValue("gilbert", out string? fullName))
         {
-            WriteLine($"offset {offset}, length {length}");
-            return this;
+            WriteLine($"Fullname is {fullName}");
         }
+        
+
+        var textToNum = new Dictionary<string,int>
+        {
+            {"one", 1},
+            {"two", 2},
+            {"three", 3},
+            {"four", 4},
+             {"one2", 1},
+            {"two2", 2},
+            {"three2", 3},
+            {"four2", 4}
+        };
+        WriteLine(textToNum["one"]);
+        foreach(var kvp in textToNum)
+        {
+            WriteLine($"{kvp.Key}={kvp.Value}");
+        }
+
+        IDictionary<char, string> letters = new Dictionary<char, string>
+        {
+            ['a'] = "A",
+            ['d'] = "D",
+            ['e'] = "E",
+            ['b'] = "B",
+            ['c'] = "C"
+        };
+        WriteLine(letters['a']);
+        WriteLine(letters['b']);
+
+        foreach(var pair in letters)
+        {
+            WriteLine(pair);
+        }
+
+        IDictionary<char, string> sortedLetters = new SortedDictionary<char, string>
+        {
+            ['a'] = "A",
+            ['d'] = "D",
+            ['e'] = "E",
+            ['b'] = "B",
+            ['c'] = "C"
+        };
+        WriteLine("*** Sorted ***");
+        foreach(var pair in sortedLetters)
+        {
+            WriteLine(pair);
+        }
+        WriteLine("*** Sorted end ***");
+
+
+        // ingore case in keys 
+        var numberNames = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase)
+        {
+            ["one"] = 1,
+            ["Two"] = 2,
+            ["thReE"] = 3
+        };
+        WriteLine(numberNames["ONE"]);
+        WriteLine(numberNames["three"]);
+
+        
+    
+
+
     }
+
+  
+  
  
 
 
 }
 
+public class UserCache 
+{
+    private readonly Dictionary<string, UserInfo> _cachedUserInfo = 
+        new Dictionary<string, UserInfo>();
 
+    public UserInfo GetInfo(string userHandle)
+    {
+        RemoveStaleCacheEntries();
+        if(!_cachedUserInfo.TryGetValue(userHandle, out UserInfo? info))
+        {
+            info = FetchUserInfo(userHandle);
+            _cachedUserInfo.Add(userHandle, info);
+        }
+        return info;
+    }
+
+    private UserInfo FetchUserInfo(string userHandle)
+    {
+        return new UserInfo();
+    }
+
+    private void RemoveStaleCacheEntries()
+    {
+
+    }
+}
+
+
+public class UserInfo 
+{}
