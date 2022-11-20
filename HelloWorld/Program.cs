0aebe7bd776b105157b26676de497e8f8c97f54b
Gilbert <gssenyonjo@gmail.com>
2022-11-20 15:32:59 +0300

    looked at virtual and override

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,22 +9,8 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Rectangle  r1 = new Rectangle()
-        {
-            Width = 5,
-            Height = 10
-        };
-        RoundedRectangle r2 = new RoundedRectangle()
-        {
-            Width = 3,
-            Height = 4
-        };
-        var arr1 = new[] { r1, r2 };
-        Array.Sort(arr1, new BoxAreaComparer());
-        foreach(var r in arr1)
-        {
-            WriteLine($"{r.Width}x{r.Height}");
-        }
+        MyBase cd = new CustomerDerived();
+        cd.Start();
     }
 
    
@@ -34,38 +20,23 @@ public class Program
 
 }
 
-
-public class Rectangle 
+public class CustomerDerived :MyBase 
 {
-    public double Width { get; set; }
-    public double Height { get; set; }
-
-    public double Area => Width * Height;
-}
-
-public class RoundedRectangle : Rectangle 
-{
-    public double CornerRadius { get; set; }
-}
-
-
-public class BoxAreaComparer : IComparer<Rectangle>
-{
-    public int Compare(Rectangle? r1, Rectangle? r2)
+    public override void Start()
     {
-        return Math.Sign(r1?.Area ?? 0 - r2?.Area ?? 0);
+        WriteLine("start");
     }
 }
-
-
-public class CornerSharpnessComparer : IComparer<RoundedRectangle>
+public class MyBase 
 {
-    public int Compare(RoundedRectangle? r1, RoundedRectangle? r2)
-    {
-        // smaller corners are sharper
-        return Math.Sign(-1 * ((r1?.CornerRadius ?? 0) - (r2?.CornerRadius ?? 0)));
-    }
+    public  virtual void Start() {WriteLine("base start");}
 }
 
 
+public abstract class House2 : AbstractHouse 
+{}
 
+public abstract class AbstractHouse 
+{
+    public abstract void Build();
+}
