114da91284da835865c1dfa07883a2f986095379
Gilbert <gssenyonjo@gmail.com>
2022-11-20 14:24:03 +0300

    looked at covariants

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,62 +9,93 @@ public class Program
 {
     static void Main(string[] args)
     {
-        IBoth2 b = new IBoth2Impl();
-        b.Base1Log();
-        b.Base2Log();
-        b.BothLog();
-        b.Both2Log();
-        
- 
+        IBase1 b1 = new Base1Impl(); 
+        // UseBase1(b1);
+        ISpecialBase1 s1 = new SpecialBase1Impl();
+        // UseBase1(s1);
+        IEnumerable<ISpecialBase1> bases = new[] { s1, s1 };
+        // WriteLine("** all bases");
+        // AllBases(bases);
+
+        ICollection<ISpecialBase1> specials = new List<ISpecialBase1>();
+        // AddBase(specials);
+
+
+        IRunner<IBase1> r1 = new Runner<IBase1>(b1);
+        PrintCurrentRunner(r1);
+
+        IRunner<ISpecialBase1> sr1 = new Runner<ISpecialBase1>(s1);
+        PrintCurrentRunner(sr1);
+
     }
+
+    public static void PrintCurrentRunner(IRunner<IBase1> r)
+    {
+        WriteLine(r.Current);
+    }
+    static void UseBase1(IBase1 b)
+    {
+        b.Base1Log();
+    }
+
+    public static void AllBases(IEnumerable<IBase1> bases)
+    {
+        foreach (IBase1 b in bases)
+        {
+            b.Base1Log();
+        }
+    }
+
+    public static void AddBase(ICollection<IBase1> bases)
+    {
+        bases.Add(new Base1Impl());
+    }
+
+
+
+}
+
+public class SpecialBase1Impl : ISpecialBase1
+{
+    public void Base1Log()
+    {
+        WriteLine("special impl of base1log");
+    }
+}
+public class Base1Impl : IBase1
+{
+    public void Base1Log()
+    {
+        WriteLine("base1 impl of base1log");
+    }
+
+}
+
+interface ISpecialBase1 : IBase1
+{
+}
+
+public interface IBase1
+{
+    void Base1Log();
+}
+
+
+
+public interface IRunner<out T>
+{
+    T Current { get; }
 
    
 
-
 }
 
-
-interface IBase1 
+public class Runner<T> : IRunner<T>
 {
-    void Base1Log()
-
-  ;
-}
-
-interface IBase2 
-{
-    void Base2Log()
+    private T _current;
+    public Runner(T t)
     {
-        WriteLine("I am base 2");
+        _current = t;
     }
-
+    public T Current => _current;
 }
-
-interface IBoth : IBase1, IBase2
-{
-    void BothLog()
-    {
-        WriteLine("I am both");
-    }
-
-}
-
-interface IBoth2 : IBoth 
-{
-    void Both2Log()
-    {
-        WriteLine("I am both 2");
-    }
-}
-
-class IBoth2Impl : IBoth2 
-{
-
-    public void Base1Log()
-    {
-        WriteLine("I am really base 1");
-    }
-}
-
-
-
