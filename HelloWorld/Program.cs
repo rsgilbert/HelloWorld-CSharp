26945c387710ebb0e4fdd46cbdee7ff30d11a769
Gilbert <gssenyonjo@gmail.com>
2022-11-23 01:34:12 +0300

    looked at handling exceptions

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,9 +11,49 @@ public class Program
 
     static void Main(string[] args)
     {
-        WriteLine(Divide(0, 0));
+        try
+        {
+            using (var r = new StreamReader("/a/Program.css"))
+            {
+                while (!r.EndOfStream)
+                {
+                    Console.WriteLine(r.ReadLine());
+                    r.Dispose();
+                }
+            }
+        }
+        
+        catch(FileNotFoundException x)
+        {
+            WriteLine($"File {x.FileName} is missing");
+            WriteLine(x.InnerException);
+            // WriteLine(x.Message);
+            // WriteLine(x.StackTrace);
+            // WriteLine(x.ToString());
+            WriteLine(x.TargetSite);
+        }
+   
+        catch(DirectoryNotFoundException x)
+        {
+            WriteLine($"Directory not found. {x.Message}");
+        }
+        catch(IOException x)
+        {}
 
-        double Divide(double x, int y)
+        try
+        {
+            WriteLine(Divide(0, 0));
+        }
+        catch (DivideByZeroException x)
+        {
+            foreach (var p in x.Data)
+            {
+                WriteLine(p);
+            }
+            WriteLine("Could not divide by zero");
+        }
+
+        double Divide(int x, int y)
         {
             return x / y;
         }
