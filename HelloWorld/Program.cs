ce2d39c915a4fa068bc09bbb0a419c79be42e7ee
Gilbert <gssenyonjo@gmail.com>
2022-11-23 07:21:56 +0300

    implemented custom exception and made it serializable

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,8 +4,7 @@ using System.Windows;
 using System.Numerics;
 using System.Diagnostics;
 using System.Net.Http;
-
-
+using System.Runtime.Serialization;
 
 public class Program
 {
@@ -14,42 +13,65 @@ public class Program
     {
         try
         {
-            WriteLine("Trying");
-            int  n = 0;
-            throw new AggregateException();
-           // int i = 4 / n;
-
+            throw new DeviceNotReadyException(DeviceStatus.Disconnected);
         }
-        catch(DivideByZeroException x)
+        catch (DeviceNotReadyException x)
         {
             WriteLine(x.Message);
-            throw; 
+            WriteLine(x.Status);
         }
-        finally
-        {
-            WriteLine("Doing  a finally");
-        }
-    
-        WriteLine(CountCommas("hey there, madam, how are you?"));
-        WriteLine(CountCommas(null));
 
-   
+
+
 
     }
 
-    public static int CountCommas(string? text)
-    {
-        if(text ==null)
-        {
-            throw new ArgumentNullException(nameof(text));
-        }
-        return text.Count(c => c == ',');
-    }
+
 
 
 
 }
 
+[Serializable]
+public class DeviceNotReadyException : InvalidOperationException
+{
+    public DeviceNotReadyException(DeviceStatus status)
+        : this("Device status must be ready", status)
+    { }
+
+    public DeviceNotReadyException(string message, DeviceStatus status)
+        : base(message)
+    {
+        Status = status;
+    }
+
+    public DeviceNotReadyException(string message, DeviceStatus status, Exception innerException)
+        : base(message, innerException)
+    {
+        Status = status;
+    }
 
 
+    public DeviceStatus Status { get; }
 
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        base.GetObjectData(info, context);
+        info.AddValue("Status", Status);
+    }
+
+    protected DeviceNotReadyException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+        Status = (DeviceStatus?) info.GetValue("Status", typeof(DeviceStatus)) ?? DeviceStatus.Initializing;
+    }
+}
+
+
+public enum DeviceStatus
+{
+    Disconnected,
+    Initializing,
+    Failed,
+    Ready
+}
