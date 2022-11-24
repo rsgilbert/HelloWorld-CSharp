fb2767337966a3a12590dd6f8dbe734f679f806f
Gilbert <gssenyonjo@gmail.com>
2022-11-24 06:43:11 +0300

    created a delegate using reflection

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,26 +5,33 @@ using System.Numerics;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Runtime.Serialization;
+using System.Reflection;
 
 public class Program
 {
 
     static void Main(string[] args)
     {
-        int[] bins = { 0, -5, -1, 50, 2, 4 };
-        bool IsGreaterThanZero(int value) => value > 0;
 
-        int firstGreater = Array.FindIndex(bins, Works.AlwaysFalse);
-        WriteLine(firstGreater);
+        var w0 = new Works(0);
+        Predicate<int> p0 = w0.Working;
+        WriteLine(p0(1));
+        WriteLine(p0(-5));
+        w0.Count = 5;
+        WriteLine(p0(1));
 
-        // We are refering to IsGreaterThanZero by name, the name IsGreaterThanZero is in this case a method case 
-        // since there maybe many methods called IsGreaterThanZero but taking different parameters
-        Predicate<int> p = IsGreaterThanZero;// new Predicate<int>(IsGreaterThanZero);
-        WriteLine(p(10));
-        var m = p(3);
+        var w1 = new Works(1);
+        Predicate<int> p1 = w1.Working;
+        WriteLine(p1(1));
+
+// creating delegate using reflection
+        MethodInfo? m1 = typeof(Works).GetMethod("Working");
+        Predicate<int> p2 = (Predicate<int>) m1.CreateDelegate(typeof(Predicate<int>), w1);
+        WriteLine(p2(11));
 
 
-    
+
+
     }
 
 
@@ -35,5 +42,16 @@ public class Program
 
 class Works
 {
+    public int Count { get; set; }
     public static bool AlwaysFalse(int k) => false;
+
+    public Works(int count)
+    {
+        Count = count;
+    }
+
+    public bool Working(int comp)
+    {
+        return comp > Count;
+    }
 }
