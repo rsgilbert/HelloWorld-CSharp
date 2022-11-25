112ab4530b2904b58c4863848e8a5c8c54db633f
Gilbert <gssenyonjo@gmail.com>
2022-11-25 20:45:48 +0300

    looked at how anonymous functions capture variables defined in the containing method

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,26 +7,42 @@ public class Program
 
     static void Main(string[] args)
     {
-        int[] bins = { 0, 0, 0, 1, 2, 3, 4 };
-        int idx = GetIndexOfFirstNonEmptyBin(bins);
-        WriteLine(idx);
-
-        EventHandler clickHandler = delegate {
-            WriteLine("Clicked");
+        var greaterThanN = new Predicate<int>[10];
+        for (int i = 0; i < greaterThanN.Length; i++)
+        {
+            var m = i;
+            greaterThanN[i] = val =>
+            {
+                WriteLine($"i is {m}");
+                return val > m;
+            };
         }
 
+        WriteLine(greaterThanN[2](10));
+        WriteLine(greaterThanN[3](1000));
+
+        WriteLine("greaterThanN2");
+        var greaterThanN2 = new Predicate<int>[4];
+        var arr = new int[] { 0, 1, 2, 3 };
+        foreach (var i in arr)
+        {
+            greaterThanN2[i] = val =>
+            {
+                WriteLine($"i is {i}");
+                return val > i;
+            };
+        }
+        WriteLine(greaterThanN2[1](3));
+        WriteLine(greaterThanN2[3](13));
+
     }
 
-    public static int GetIndexOfFirstNonEmptyBin(int[] bins)
+    public static Predicate<int> IsGreaterThan(int threshold)
     {
-        return Array.FindIndex(
-            bins,
-            (int v) =>
-            {
-                return v > 0;
-            }
-        );
+        return v => v > threshold;
     }
+
+    public static Predicate<int> IsGreaterThan2(int threshold) => val => val > threshold;
 
 
 
