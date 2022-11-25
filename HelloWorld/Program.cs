1a94f0cb1c66b3f5bcbfe9c4248d64f4c461f86f
Gilbert <gssenyonjo@gmail.com>
2022-11-25 21:13:15 +0300

    Defined a lambda expression

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Console;
 using static System.Math;
+using System.Linq.Expressions;
 
 public class Program
 {
@@ -7,42 +8,11 @@ public class Program
 
     static void Main(string[] args)
     {
-        var greaterThanN = new Predicate<int>[10];
-        for (int i = 0; i < greaterThanN.Length; i++)
-        {
-            var m = i;
-            greaterThanN[i] = val =>
-            {
-                WriteLine($"i is {m}");
-                return val > m;
-            };
-        }
-
-        WriteLine(greaterThanN[2](10));
-        WriteLine(greaterThanN[3](1000));
-
-        WriteLine("greaterThanN2");
-        var greaterThanN2 = new Predicate<int>[4];
-        var arr = new int[] { 0, 1, 2, 3 };
-        foreach (var i in arr)
-        {
-            greaterThanN2[i] = val =>
-            {
-                WriteLine($"i is {i}");
-                return val > i;
-            };
-        }
-        WriteLine(greaterThanN2[1](3));
-        WriteLine(greaterThanN2[3](13));
+        Expression<Func<int, bool>> greaterThanZero = value => value > 0;
+WriteLine(greaterThanZero);
+     
 
     }
-
-    public static Predicate<int> IsGreaterThan(int threshold)
-    {
-        return v => v > threshold;
-    }
-
-    public static Predicate<int> IsGreaterThan2(int threshold) => val => val > threshold;
 
 
 
