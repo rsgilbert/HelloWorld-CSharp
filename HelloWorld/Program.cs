2993812bed16c954bb4a6e90f3dd4cec0111051c
Gilbert <gssenyonjo@gmail.com>
2022-11-25 21:28:22 +0300

    Looking at defining and using events

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,8 +8,18 @@ public class Program
 
     static void Main(string[] args)
     {
-        Expression<Func<int, bool>> greaterThanZero = value => value > 0;
-WriteLine(greaterThanZero);
+        var unev = new UnEventful();
+        unev.Announcement += s => 
+        {
+            WriteLine($"Announcement made {s}");
+        };
+        unev.Announcement += s => 
+        {
+            WriteLine($"Incase you didnt here: {s}");
+        };
+
+        unev.add_Annoucement2(s => WriteLine($"I want you to know that {s}"));
+        unev.Announce("Someone is getting married tomorrow!");
      
 
     }
@@ -20,3 +30,20 @@ WriteLine(greaterThanZero);
 }
 
 
+public class UnEventful 
+{
+    public event Action<string>? Announcement;
+
+    public void Announce(string message)
+    {
+        Announcement?.Invoke(message);
+        Announcement2?.Invoke(message);
+    }
+
+    private Action<string>? Announcement2;
+
+    public void add_Annoucement2(Action<string> action)
+    {
+        Announcement2 += action;
+    }
+}
