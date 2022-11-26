25de8a944f799d24925a0ae270366dc3987dadb5
Gilbert <gssenyonjo@gmail.com>
2022-11-26 14:28:52 +0300

    implemented BigIntegerLinqProvider that returns IEnumerable in the Where method

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,24 +1,35 @@
 ﻿using static System.Console;
 using static System.Math;
 using System;
-
+using System.Collections.Generic;
+using System.Numerics;
+// using System.Linq;
 public class Program
 {
     static void Main(string[] args)
     {
-
-        string[] colors = { "green", "yellow", "indigo", "violet", "blue", "red", "Black", "Brown" };
-
-        //  b colors 
-        var bColors = from color in colors
-                    //   let lowercaseColor = color.ToLower()
-                      where color.ToLower().StartsWith("b")
-                      select color.AsSpan(0, 3).ToString();
-        foreach (var c in bColors)
+        var evenFib = from n in Fibonacci() 
+            where n % 2 == 0 
+            select n;
+        foreach(BigInteger n in evenFib)
         {
-            WriteLine(c);
+            if(n > 1000) return;
+            WriteLine(n);
         }
+    }
 
+    static IEnumerable<BigInteger> Fibonacci()
+    {
+        BigInteger n1 = 1;
+        BigInteger n2 = 1;
+        yield return n1; 
+        while(true)
+        {
+            yield return n2;
+            BigInteger temp = n1;
+            n1 = n2;
+            n2 = temp + n2;
+        }
     }
 
 
@@ -26,21 +37,37 @@ public class Program
 
 }
 
-public static class CustomLinqProvider
+public static class BigIntegerLinqProvider 
 {
-    public static string[] Where(this string[] arr, Predicate<string> filter)
+    public static IEnumerable<BigInteger> Where(this IEnumerable<BigInteger> en, Predicate<BigInteger> filter)
     {
-        WriteLine("Filtering array");
-        return Array.FindAll(arr, filter);
-    }
-    public static T[] Select<T>(this string[] arr, Func<string, T> map)
-    {
-        WriteLine("Selecting...");
-        var result = new T[arr.Length];
-        for(int i = 0; i < arr.Length; i++)
+        WriteLine("Filtering");
+        foreach(var bi in en)
         {
-            result[i] = map(arr[i]);
+            if(filter(bi))
+            {
+                yield return bi;
+            }
         }
-        return result;
+        
     }
 }
+
+// public static class CustomLinqProvider
+// {
+//     public static string[] Where(this string[] arr, Predicate<string> filter)
+//     {
+//         WriteLine("Filtering array");
+//         return Array.FindAll(arr, filter);
+//     }
+//     public static T[] Select<T>(this string[] arr, Func<string, T> map)
+//     {
+//         WriteLine("Selecting...");
+//         var result = new T[arr.Length];
+//         for(int i = 0; i < arr.Length; i++)
+//         {
+//             result[i] = map(arr[i]);
+//         }
+//         return result;
+//     }
+// }
