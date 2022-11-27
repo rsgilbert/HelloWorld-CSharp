4c560126af2fad3ef403bc9f2d4ff1a7dbdecc2e
Gilbert <gssenyonjo@gmail.com>
2022-11-27 15:42:48 +0300

    Looked at the join operator

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,83 +8,76 @@ public class Program
 {
     static void Main(string[] args)
     {
+        var studentsAndCourses =
+            from choice in CourseChoice.ChoiceCatalog
+            join course in Course.Catalog
+            on choice.Number equals course.Number
+            orderby choice.Number
+            select new { course.Number, course.Title, choice.StudentId };
+        Print(studentsAndCourses);
 
-        var subjectGroups = from course in Course.Catalog
-                            group course.Title by course.Category;
+        var orders = new[]
+        {
+            new { OrderId=1, Description="Pilao"},
+            new { OrderId=2, Description="Chips"},
+            new { OrderId=3, Description="Macron"}
+        };
+        var orderLines = new[]
+        {
+            new { DocumentNo =1, Qty=2,Description="Rice"},
+            new { DocumentNo=2, Qty=1, Description="Chip slices"},
+            new {DocumentNo=2, Qty=2, Description="Tomato sauce"},
+            new {DocumentNo=3, Qty=1, Description="Macron"},
+            new {DocumentNo=3, Qty=1, Description="Cola"}
+        };
 
-        subjectGroups = Course.Catalog.GroupBy(c => c.Category, c => c.PublicationDate.ToString());
-
-        // foreach(var group in subjectGroups)
-        // {
-        //     WriteLine($"Category: {group.Key}");
-        //     Print(group);
-        // }
-
-        string[] names = { "Jimmy", "James", "Paddy", "Maggie", "Pedro", "Rooney", "Suzan", "Ben" };
-        var groupedNames =
-            from name in names
-            group name by new { name.Length, firstChar= name[0] } into namesByLength
-            let namesByLengthCount = namesByLength.Count()
-            orderby namesByLengthCount
-            select (namesByLength.Key, namesByLengthCount);
-
-        Print(groupedNames);
+        var allOrders = 
+            from order in orders 
+            join orderLine in orderLines 
+            on order.OrderId equals orderLine.DocumentNo 
+            select new { order.OrderId, order.Description, LineDescription=orderLine.Description, orderLine.Qty};
+        Print(allOrders);
 
 
 
     }
 
     static void Print<T>(IEnumerable<T> enumerable)
-    {
-        foreach (var item in enumerable)
         {
-            WriteLine(item);
+            foreach (var item in enumerable)
+            {
+                WriteLine(item);
+            }
         }
+
+
+
+
+
     }
 
 
-
-
-
-}
-
-public class Person
-{
-    public override string ToString()
+    public class Course
     {
-        return "I am a person";
-    }
-}
-
-public class Female : Person
-{
-    public override string ToString()
-    {
-        return "I am a female person";
-    }
-}
-
-public class Course
-{
-    public string Title { get; set; } = "";
-    public string Category { get; set; } = "";
-    public int Number { get; set; }
-    public DateTime PublicationDate { get; set; }
-    public TimeSpan Duration { get; set; }
-    public override string ToString()
-    {
-        return $"Course(Title={Title}, Category={Category})";
-    }
+        public string Title { get; set; } = "";
+        public string Category { get; set; } = "";
+        public int Number { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public override string ToString()
+        {
+            return $"Course(Title={Title}, Category={Category})";
+        }
 
 
 
-    public static IEnumerable<Course> ShortEvenIdxCourses()
-    {
-        return Catalog
-            .Where((course, idx) => (idx % 2 == 0 && course.Duration.TotalHours < 3));
-    }
-    public static readonly Course[] Catalog =
-    {
+        public static IEnumerable<Course> ShortEvenIdxCourses()
+        {
+            return Catalog
+                .Where((course, idx) => (idx % 2 == 0 && course.Duration.TotalHours < 3));
+        }
+        public static readonly Course[] Catalog =
+        {
         new Course
         {
             Title = "Elements of Geometry",
@@ -96,7 +89,7 @@ public class Course
 
         new Course
         {
-               Title = "Data Structures and Algorithms",
+            Title = "Data Structures and Algorithms",
             Category = "CS",
             Number = 104,
             PublicationDate = new DateTime(2017,11, 15),
@@ -118,13 +111,26 @@ public class Course
             PublicationDate = new DateTime(2015, 5, 21),
             Duration = TimeSpan.FromHours(2),
         },
-        new Course
-        {
-            Title = "Theory of Computation",
-            Category = "CS",
-            Number = 104,
-            PublicationDate = new DateTime(2015, 5, 21),
-            Duration = TimeSpan.FromHours(5),
-        }
+
     };
-}
+
+
+    }
+
+    public class CourseChoice
+    {
+        public int StudentId { get; set; }
+        public int Number { get; set; }
+
+        public static CourseChoice[] ChoiceCatalog =
+        {
+        new CourseChoice{ StudentId = 1, Number = 101 },
+        new CourseChoice { StudentId = 1, Number = 103 },
+        new CourseChoice{ StudentId = 2, Number = 101 },
+        new CourseChoice { StudentId = 2, Number = 105},
+        new CourseChoice{ StudentId = 3, Number = 101 },
+        new CourseChoice { StudentId = 4, Number = 103 },
+        new CourseChoice{ StudentId = 4, Number = 104 },
+        new CourseChoice { StudentId = 4, Number = 105},
+    };
+    }
